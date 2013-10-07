f7c677c01379ab27801bca434d423c87b061ee2d
Brain90 <gibransyah@gmail.com>
2013-10-07 17:03:31 +0700

    Versi java

---
+++ PenjagaTitikTengah/PenjagaTitikTengah.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class PenjagaTitikTengah {
+  
+  int[] POLA_3x3 = {1,4,3,2};
+  int median = 0;
+  int[] pola = {};
+  int total8Angka = 0;
+
+  private void cariJumlahAngkaDiSekelilingMedian() {
+	
+  }
+
+  public static void main (String []args) {
+	System.out.println("Masih kosong bro....");
+  }
+
+}
