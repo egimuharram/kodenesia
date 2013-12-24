00934fdea111b6a3aa83769d1066a9d1c4f1b2df
wildanr <wildan.rahman@gmail.com>
2013-12-24 08:40:10 +0700

    shorter version + remove unspecified output (double newline, case number)

--- PesanRahasia/PesanRahasia.py
+++ PesanRahasia/PesanRahasia.py
@@ -1,45 +1,28 @@
 #!/usr/bin/python -tt
 import sys
 
+
 class PesanRahasia:
+   def bongkar(self):
+      pembaca = open('input.8', 'r')
+      n = int(pembaca.readline())
+      for i in range(1, n + 1):
+         baris = pembaca.readline().strip()
+         print self.terjemahkan(baris)
 
-  kamus = {"a": "n", "b": "o", "c": "p", "d": "q",
-           "e": "r", "f": "s", "g": "t", "h": "u",
-           "i": "v", "j": "w", "k": "x", "l": "y",
-           "m": "z"}
+   def terjemahkan(self, origin):
+      result = []
+      for c in origin:
+         if not c.isalpha():
+            result.append(c)
+            continue
 
-  def bongkar(self):
-    hasil = ''
-    pembaca = open('input.8', 'r')
-    n = int(pembaca.readline()) 
-    for i in range(1, n+1):
-      baris = pembaca.readline().strip()
-      potonganHuruf = list(baris) 
-      for huruf in potonganHuruf:
-        hasil += self.terjemahkan(huruf)
-      print 'Case #%d: %s' % (i, hasil)+'\n'
-      hasil = ''    
+         base = 'a'
+         num = ord(c) - ord(base)
+         new = chr(ord(base) + ((num + 13) % 26))
+         result.append(new)
 
-  def terjemahkan(self,karakter):
-    ketemu = 0
-    # Indeks pencarian = key
-    for key,values in self.kamus.iteritems():
-      if karakter == key:
-        terjemahanHuruf = values
-        ketemu = 1
-    
-    # Indeks pencarian = values
-    for key,values in self.kamus.iteritems():
-      if karakter == values:
-        terjemahanHuruf = key
-        ketemu = 1
-    
-    # Kesimpulan hasil pencarian
-    if ketemu == 1:
-      return terjemahanHuruf
-    else:
-      return karakter
+      return ''.join(result)
 
-    
 PesanRahasia = PesanRahasia()
 PesanRahasia.bongkar()
