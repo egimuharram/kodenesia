3c3db497e60ff7d1fc7c314149db6f782925f35d
Brain90 <gibransyah@gmail.com>
2014-04-11 06:18:47 +0700

    init

---
+++ 2_UrutanTerpanjang/UrutanTerpanjang.php
@@ -0,0 +1,31 @@
+<?php
+
+class UrutanTerpanjang {
+
+
+    function __construct(){
+
+        $sequence = array(4,8,7,1,12,13,99,43,44);
+        $sequenceGroup = 0;
+        $sequenceGroupContainer = array();
+
+        asort($sequence);
+        $seqLength = count($sequence);
+        for ($i=0; $i<$seqLength; $i++) {
+            if ($sequence[$k+1] - $sequence[$k] == 1){
+            } else {
+            }
+        }
+    }
+
+
+    function seqGrouping() {
+
+    }
+
+}
+
+$urut = new UrutanTerpanjang;
+
+
+?>
